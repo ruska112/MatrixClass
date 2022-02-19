0bc25211e219807df12147e4aacd1e0539221e57
ruska112 <94182636+ruska112@users.noreply.github.com>
2022-02-19 18:20:23 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,16 +18,26 @@ class matrix_2d
         // Коструктор копирования с добавлением "_copy" к имени(name) матрицы
         matrix_2d(const matrix_2d&);
 
+        // Конструктор матрицы, являющейся суммой двух других матриц
+        matrix_2d(const matrix_2d&, const matrix_2d&);
+
         // Деструктор
         ~matrix_2d();
 
         // Сеттер элемента i-той строки j-того столбца
-        void SetIJ(int, int, double);
+        int SetIJ(int, int, double);
 
-        // Геттеры
-        double GetIJ(int, int);
+        // Геттер элемента i-той строки j-того столбца
+        int GetIJ(int, int, double&) const;
 
-        void GetFull();
+        // Геттер кол-ва строк
+        int GetNlines() const;
+
+        // Геттер кол-ва столбцов
+        int GetNcolumns() const;
+
+        // Вывод всей матрицы
+        int PrintFull() const;
 };
 
 matrix_2d::matrix_2d()
@@ -96,33 +106,50 @@ matrix_2d::~matrix_2d()
     delete[] array;
 }
 
-void matrix_2d::SetIJ(int i, int j, double value)
+int matrix_2d::SetIJ(int i, int j, double value)
 {
-    if ( i >= 0 && j >= 0)
+    int result = 1;
+
+    if ( (i >= 0) && (j >= 0) && (i <= nlines) && (j <= ncolumns) )
     {
         array[i][j] = value;
+
+        result = 0;
     }
-    else
-    {
-        return;
-    }
+    
+    return result;
 }
 
-double matrix_2d::GetIJ(int i, int j)
+int matrix_2d::GetIJ(int i, int j, double& t_ij) const
 {
-    if ( i >= 0 && j >= 0)
+    int result = 1;
+
+    if ( (i >= 0) && (j >= 0) && (i <= nlines) && (j <= ncolumns) )
     {
-        return array[i][j];
+        t_ij = array[i][j];
+        result = 0;
     }
-    else
-    {
-        return 0x0001;
-    }
+
+    return result;
 }
 
-void matrix_2d::GetFull()
+int matrix_2d::GetNlines() const
 {
-    if(nlines > 0 && ncolumns > 0)
+    return nlines;
+}
+
+int matrix_2d::GetNcolumns() const
+{
+    return ncolumns;
+}
+
+int matrix_2d::PrintFull() const
+{
+    int result = 1;
+
+    std::cout << name << std::endl;
+
+    if( (nlines > 0) && (ncolumns > 0) )
     {
         int i, j;
         for(i = 0; i < nlines; i++)
@@ -133,18 +160,73 @@ void matrix_2d::GetFull()
             }
             printf("\n");
         }
+        printf("\n");
+
+        result = 0;
+    }
+
+    return result;
+}
+
+matrix_2d::matrix_2d(const matrix_2d& m0, const matrix_2d& m1)
+{
+    if ( (m0.nlines == m1.nlines) && (m0.ncolumns == m1.ncolumns) )
+    {
+        name = "sum_" + m0.name + "_" + m1.name;
+        nlines = m0.nlines;
+        ncolumns = m0.ncolumns;
+        array = new double *[nlines];
+        
+        int i, j;
+        double tmp0, tmp1;
+
+        for (i = 0; i < nlines; i++)
+        {
+            array[i] = new double[ncolumns];
+            for (j = 0; j < ncolumns; j++)
+            {
+                
+                m0.GetIJ(i, j, tmp0);
+                m1.GetIJ(i, j, tmp1);
+                
+                array[i][j] = tmp0 + tmp1;
+            }
+        }
     }
 }
 
 int main()
 {   
-    matrix_2d m0("m0", 3, 3);
+    matrix_2d m0("M_0", 3, 3);
+    matrix_2d m1("M_1", 3, 3);
+    matrix_2d sum_m0_m1;
 
-    m0.SetIJ(0, 0, 112);
-    m0.SetIJ(1, 1, 112);
-    m0.SetIJ(2, 2, 112);
+    int i, j;
+    double tmp;
 
-    m0.GetFull();
+    for (i = 0; i < m0.GetNlines(); i++)
+    {
+        for (j = 0; j < m0.GetNcolumns(); j++)
+        {
+            scanf("%lf", &tmp);
+            m0.SetIJ(i, j, tmp);
+        }
+    }
+    m0.PrintFull();
+
+    for (i = 0; i < m1.GetNlines(); i++)
+    {
+        for (j = 0; j < m1.GetNcolumns(); j++)
+        {
+            scanf("%lf", &tmp);
+            m1.SetIJ(i, j, tmp);
+        }
+    }
+    m1.PrintFull();
+
+    matrix_2d sum(m0, m1);
+
+    sum.PrintFull();
 
     return 0;
 }
